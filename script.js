9fe2534d1efc6a1d3e0ece133905ee5bc3d3f506
Blessing Emejulu <69702136+BlessingEmejulu@users.noreply.github.com>
2023-12-05 03:40:45 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,28 @@ let advice_id = document.getElementById("advice-id");
 let advice_quote = document.getElementById("advice-quote");
 let button = document.getElementById("get-quote");
 
-let adviceNum = "";
-let fetchedAdvice = "";
+// Function to update the UI with advice
+function updateAdvice(data) {
+    // --- Assign advice number---
+    adviceNum = data.slip.id;
+    // --- Assign advice quote ---
+    fetchedAdvice = data.slip.advice;
+    // --- Update text advice text on screen ---
+    advice_id.textContent = `#${adviceNum}`;
+    advice_quote.textContent = `"${fetchedAdvice}"`;
+    // Fadein quote and number ---
+    advice_id.style.setProperty("opacity", 1);
+    advice_quote.style.setProperty("opacity", 1);
+}
+
+// FUNCTION TO HANDLE ERRORS
+function handleErrors(error) {
+    // Log the error to the console for debugging purposes
+    console.error('Error fetching advice:', error);
+    // Display a user-friendly message on the screen
+    advice_id.textContent = 'Error';
+    advice_quote.textContent = 'Failed to fetch advice. Please try again later.';
+}
 
 // ------ EVENTS -------
 button.addEventListener("click", getAdvice);
@@ -12,20 +32,25 @@ button.addEventListener("click", getAdvice);
 // ----- GETTING ADVICE QUOTE FROM THE API
 function getAdvice() {
     fetch("https://api.adviceslip.com/advice")
-        .then(resp => resp.json())
+        .then(resp => {
+            if (!resp.ok) {
+                // Handle specific HTTP status codes
+                if (resp.status === 404) {
+                    throw new Error('Advice not found');
+                } else if (resp.status === 500) {
+                    throw new Error('Server error');
+                } else {
+                    throw new Error('Network response was not ok');
+                }
+            }
+            return resp.json();
+        })
         .then(data => {
-            // --- ASSIGN QUOTE NUMBER ---
-            adviceNum = data.slip.id;
-            // --- ASSIGN ADVICE QUOTE ---
-            fetchedAdvice = data.slip.advice;
-            // --- UPDATE ADVICE TEXT ON SCREEN ---
-            advice_id.textContent = `#${adviceNum}`;
-            advice_quote.textContent = `"${fetchedAdvice}"`;
-            // FADEIN QUOTE AND # ---
-            advice_id.style.setProperty("opacity", 1);
-            advice_quote.style.setProperty("opacity", 1);
-        });
-} // --- END OF getAdvice FUNCTION ---
+            // Call the function to update the UI with advice
+            updateAdvice(data);
+        })
+        .catch(handleErrors);
+}
 
 // ----- FETCH A QUOTE IMMEDIATELY ON PAGELOAD -----
 getAdvice();
